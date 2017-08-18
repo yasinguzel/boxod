48c7141e10c29860d24df2752626ab3b9c2b969b
guzelyasin <guzelyasin@icloud.com>
2017-08-18 15:55:50 +0300

    AD System Added

---
+++ Assets/Scripts/ADSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Heyzap;
+
+public class ADSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		HeyzapAds.Start("9a5b94190642abf2b815c547052624d3", HeyzapAds.FLAG_NO_OPTIONS);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void testAD(){
+		HeyzapAds.ShowMediationTestSuite();
+	}
+}
